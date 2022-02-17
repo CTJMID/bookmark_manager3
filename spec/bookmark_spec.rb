950333c620446c7af08b85b1e840a5f7e1964bc4
Chris Middleton <ctjmiddleton@gmail.com>
2022-02-17 17:12:02 +0000

    Adding  links to bookmark

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,28 +1,34 @@
 require 'bookmark'
+require 'database_helper'
 
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
       connection = PG.connect( dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers Academy');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.destroyallsoftware.com', 'Destroy Software');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'Google');")
 
       bookmarks = Bookmark.all
-      
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks[0].title).to eq 'Makers Academy'
+      # expect(bookmarks).to include('Destroy Software')
+      # expect(bookmarks).to include('Google')
     end 
   end
 
   describe '#create' do
    it 'creates a new bookmark' do
-    Bookmark.create(url: 'www.wikipedia.org')
-    expect(Bookmark.all).to include('www.wikipedia.org')
-   end
+    bookmark = Bookmark.create(url: 'www.wikipedia.org', title: 'Wikipedia')
+    persisted_data = persisted_data(id: bookmark.id)
+
+    expect(bookmark).to be_a Bookmark
+    expect(bookmark.id).to eq persisted_data['id']
+    expect(bookmark.title).to eq 'Wikipedia'
+    expect(bookmark.url).to eq 'www.wikipedia.org'
   end
+end
 
 end
--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,7 +2,8 @@ feature 'Adding bookmarks' do
     scenario 'A user can add bookmarks and see it when listed' do
         visit '/bookmarks/new'
         fill_in('url', with: 'www.wikipedia.org')
+        fill_in('title', with: 'Wikipedia')
         click_button('Add')
-        expect(page).to have_content('www.wikipedia.org')
+        expect(page).to have_link('Wikipedia', href: 'www.wikipedia.org')
     end
 end
